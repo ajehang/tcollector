c03dd03eb6b5e0a6f2492bf6ce4976c36a597d83
ali imran jehangiri <ali.jehangiri@gwdg.de>
2012-05-23 16:46:04 +0200

    added piotr libvirt and own cgroup collector

---
+++ collectors/0/cg-qemu-sub.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python                                                                                                                                                                                                                                                                  
+import os
+import sys
+import time
+
+rootdir="/dev/cgroups/cpuacct/sysdefault/libvirt/qemu"
+def collectData(groups):
+        cpu_usage = 0;
+	user = 0;
+	system = 0;
+        ts = int(time.time())
+# total CPU time (in nanoseconds) consumed by all tasks in this cgroup
+        with open('/dev/cgroups/cpuacct/sysdefault/libvirt/qemu/%s/cpuacct.usage' % (groups,), 'r') as f:
+                cpu_usage = int(f.readline())
+                print("cgroup.domain.cpuacct.usage %d %s vm=%s" % (ts, cpu_usage, groups))
+# CPU time is reported in the units defined by the USER_HZ variable. 				 
+	with open('/dev/cgroups/cpuacct/sysdefault/libvirt/qemu/%s/cpuacct.stat' % (groups,), 'r') as f:
+		lines = f.read().splitlines()
+				
+	for line in lines:
+		data = line.split()
+		if data[0] == "user":
+			user = int(data[1])
+			print("cgroup.domain.cpuacct.cputime %d %s vm=%s mode=user" % (ts, user, groups))
+		elif data[0] == "system":
+			system = int(data[1])
+			print("cgroup.domain.cpuacct.cputime %d %s vm=%s mode=system" % (ts, system, groups))
+				
+# total_swap reports the total swap usage by the cgroup and all its child groups
+# total_cache reports the total page cache, including tmpfs (shmem), in bytes used by cgroup and all its child groups
+# total_rss anonymous and swap cache, not including tmpfs (shmem), in bytesused by cgroup and all its child groups
+	with open('/dev/cgroups/memory/sysdefault/libvirt/qemu/%s/memory.stat' % (groups,), 'r') as f:
+		lines = f.read().splitlines()
+	for line in lines:
+                data = line.split()
+                if data[0] == "total_rss":
+                        mem_rss = int(data[1])
+			print("cgroup.domain.memory.total_rss %d %s vm=%s" % (ts, mem_rss, groups))
+		elif data[0] == "total_cache":
+                        mem_cache = int(data[1])
+			print("cgroup.domain.memory.total_cache %d %s vm=%s" % (ts, mem_cache, groups))
+		elif data[0] == "total_swap":
+                        mem_swap = int(data[1])
+			print("cgroup.domain.memory.total_swap %d %s vm=%s" % (ts, mem_swap, groups))
+
+
+def main():
+        for root, subFolders, files in os.walk(rootdir):
+                        for folder in subFolders:
+                                collectData(folder)
+if __name__ == "__main__":
+    main()
+
---
+++ collectors/0/libvirtstat.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2012 Piotr Kasprzak 
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.  You should have received a copy
+# of the GNU Lesser General Public License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
+#
+"""VM stats based on libvirt for TSDB"""
+
+# "virt.domain.state": 			state of libvirt domain:
+
+# 	enum virDomainState {
+# 		VIR_DOMAIN_NOSTATE	= 	0	: no state
+# 		VIR_DOMAIN_RUNNING	= 	1	: the domain is running
+# 		VIR_DOMAIN_BLOCKED	= 	2	: the domain is blocked on resource
+# 		VIR_DOMAIN_PAUSED	= 	3	: the domain is paused by user
+# 		VIR_DOMAIN_SHUTDOWN	= 	4	: the domain is being shut down
+# 		VIR_DOMAIN_SHUTOFF	= 	5	: the domain is shut off
+# 		VIR_DOMAIN_CRASHED	= 	6	: the domain is crashed
+# 		VIR_DOMAIN_PMSUSPENDED	= 	7	: NB: this enum value will increase over time as new events are added to the libvirt API. It reflects the last state supported by this version of the libvirt API.
+# 		VIR_DOMAIN_LAST		= 	8
+# 	}
+
+# "virt.domain.max_mem": 		the maximum memory in KBytes allowed
+# "virt.domain.memory": 		the memory in KBytes used by the domain
+# "virt.domain.nr_virt_cpu":		the number of virtual CPUs for the domain
+# "virt.domain.cpu_time":		the CPU time used in milliseconds
+
+# "virt.domain.interface.rx_bytes":
+# "virt.domain.interface.rx_packets":
+# "virt.domain.interface.rx_errs":
+# "virt.domain.interface.rx_drop":
+# "virt.domain.interface.tx_bytes":
+# "virt.domain.interface.tx_packets":
+# "virt.domain.interface.tx_errs":
+# "virt.domain.interface.tx_drop":
+
+# "virt.domain.disk.rd_req":		number of read requests
+# "virt.domain.disk.rd_bytes":		number of read bytes
+# "virt.domain.disk.wr_req":		number of write requests
+# "virt.domain.disk.wr_bytes":		number of written bytes
+# "virt.domain.disk.errs":		In Xen this returns the mysterious 'oo_req'.
+
+import os
+import sys
+import time
+import socket
+import re
+
+import libvirt
+import libxml2
+
+# Poll interval in seconds
+
+COLLECTION_INTERVAL = 1
+
+def get_devices(document, xpath):
+    """get list of specified devices from XML definition"""
+
+    context 	= document.xpathNewContext()
+    devices 	= []
+    try:
+        ret = context.xpathEval(xpath)
+        for node in ret:
+            device = None
+            for child in node.children:
+                if child.name == "target":
+                    device = child.prop("dev")
+            if device == None:
+                continue
+            devices.append(device)
+    finally:
+        if context != None:
+            context.xpathFreeContext()
+    return devices
+
+
+def main():
+    """libvirtstat main loop"""
+
+    while True:
+
+        ts = int(time.time())
+        
+	lv = libvirt.openReadOnly("qemu:///system")
+
+	domain_ids = lv.listDomainsID()
+
+	for domain_id in domain_ids:
+
+		domain 		= lv.lookupByID(domain_id)
+		domain_name	= domain.name()
+		infos 		= domain.info()
+
+		# Parse domain XML definition for devices
+
+    		xml 		= domain.XMLDesc(0)
+    		document 	= None
+    		try:
+        		document 	= libxml2.parseDoc(xml) 
+                	interfaces 	= get_devices(document, "/domain/devices/interface")
+			disks		= get_devices(document, "/domain/devices/disk")
+
+    		except:
+			continue
+		finally:
+			if document != None:
+				document.freeDoc()
+
+		# Data from "virDomainInfo" structure
+
+		print("virt.domain.state %d %s vm=%s" 		% (ts, infos[0], domain_name))
+		print("virt.domain.max_mem %d %s vm=%s"		% (ts, infos[1], domain_name))
+		print("virt.domain.memory %d %s vm=%s"		% (ts, infos[2], domain_name))
+
+                print("virt.domain.nr_virt_cpu %d %s vm=%s"  	% (ts, infos[3], domain_name))
+                print("virt.domain.cpu_time %d %s vm=%s"  	% (ts, infos[4] / 1000000, domain_name))
+
+		# Data from "virDomainInterfaceStatsStruct" structure
+
+		for interface in interfaces:
+			
+			interface_stats = domain.interfaceStats(interface)
+
+			print("virt.domain.interface.rx_bytes %d %s vm=%s interface=%s" 	% (ts, interface_stats[0], domain_name, interface))
+                        print("virt.domain.interface.rx_packets %d %s vm=%s interface=%s" 	% (ts, interface_stats[1], domain_name, interface))
+                        print("virt.domain.interface.rx_errs %d %s vm=%s interface=%s" 		% (ts, interface_stats[2], domain_name, interface))
+                        print("virt.domain.interface.rx_drop %d %s vm=%s interface=%s" 		% (ts, interface_stats[3], domain_name, interface))
+                        print("virt.domain.interface.tx_bytes %d %s vm=%s interface=%s" 	% (ts, interface_stats[4], domain_name, interface))
+                        print("virt.domain.interface.tx_packets %d %s vm=%s interface=%s" 	% (ts, interface_stats[5], domain_name, interface))
+                        print("virt.domain.interface.tx_errs %d %s vm=%s interface=%s" 		% (ts, interface_stats[6], domain_name, interface))
+                        print("virt.domain.interface.tx_drop %d %s vm=%s interface=%s" 		% (ts, interface_stats[7], domain_name, interface))
+
+		# Data from "virDomainBlockStatsStruct" structure
+
+		for disk in disks:
+
+			block_stats = domain.blockStats(disk)
+
+			print("virt.domain.disk.rd_req %d %s vm=%s disk=%s"			% (ts, block_stats[0], domain_name, disk))
+                        print("virt.domain.disk.rd_bytes %d %s vm=%s disk=%s"                  	% (ts, block_stats[1], domain_name, disk))
+                        print("virt.domain.disk.wr_req %d %s vm=%s disk=%s"                   	% (ts, block_stats[2], domain_name, disk))
+                        print("virt.domain.disk.wr_bytes %d %s vm=%s disk=%s"                 	% (ts, block_stats[3], domain_name, disk))
+                        print("virt.domain.disk.errs %d %s vm=%s disk=%s"                     	% (ts, block_stats[4], domain_name, disk))
+
+        sys.stdout.flush()
+        time.sleep(COLLECTION_INTERVAL)
+
+if __name__ == "__main__":
+    main()
+
